1bd43ebd32a4c596bdd19324b59bcb3255cee424
TanyaMashamba01 <mildredtanyamashamba01@gmail.com>
2025-03-17 22:33:12 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -28,8 +28,8 @@ function randomMegaId(length = 6, numberLength = 4) {
 async function uploadCredsToMega(credsPath) {
     try {
         const storage = await new Storage({
-            email: 'Joshuamamboztech@gmail.com', // Your Mega A/c Email Here
-            password: 'mambojosh@201' // Your Mega A/c Password Here
+            email: 'nexusxd.bot@gmail.com', // Your Mega A/c Email Here
+            password: 'malvin266' // Your Mega A/c Password Here
         }).ready;
         console.log('Mega storage initialized.');
 
